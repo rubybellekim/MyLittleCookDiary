0180f0e716bb3e9e7824070898c3fef72b75a534
rubythebelle <rubybellekim@gmail.com>
2025-02-18 15:03:08 -0800

    Initial Commit

---
+++ MyLittleCookDiary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyLittleCookDiary
+//
+//  Created by Ruby Kim on 2025-02-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyLittleCookDiary/MyLittleCookDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyLittleCookDiaryApp.swift
+//  MyLittleCookDiary
+//
+//  Created by Ruby Kim on 2025-02-18.
+//
+
+import SwiftUI
+
+@main
+struct MyLittleCookDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
